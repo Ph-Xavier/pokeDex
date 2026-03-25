6586a470e070a5a57b62c9c37891e228e01e1641
Pedro Xavier <pedro_xavier9@outlook.com>
2026-03-25 14:16:26 -0300

    feat: implementar rotas iniciais

---
+++ src/routes.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Login from "./pages/login";
+import Cadastro from "./pages/cadastro";
+import Home from "./pages/home";
+import Pokemon from "./pages/pokemon";
+
+const Stack = createStackNavigator();
+
+export default function Routes() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        {/* <Stack.Screen
+          name="login"
+          component={Login}
+          options={{
+            title: "Login",
+            headerTitleAlign: "center",
+            headerTitleStyle: { color: "#fff", fontWeight: "bold" },
+          }}
+        />
+        <Stack.Screen
+          name="cadastro"
+          component={Cadastro}
+          options={{
+            title: "Cadastro Pokédex",
+            headerTitleAlign: "center",
+            headerTitleStyle: { color: "#fff", fontWeight: "bold" },
+          }}
+        />
+        <Stack.Screen
+          name="home"
+          component={Home}
+          options={{
+            title: "Pokédex",
+            headerTitleAlign: "center",
+            headerTitleStyle: { color: "#fff", fontWeight: "bold" },
+          }}
+        />
+        <Stack.Screen
+          name="pokemon"
+          component={Pokemon}
+          options={{
+            title: "Pokémon",
+            headerTitleAlign: "center",
+            headerTitleStyle: { color: "#fff", fontWeight: "bold" },
+          }}
+        /> */}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
